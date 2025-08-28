4744ab6a7363d4494c631b9a45e25ff7270c6748
Pirodax <ludo.bergeron@icloud.com>
2025-08-28 03:40:19 +0200

    fix: correction du titre de la page d'acceuil

--- exercice-front/components/ui/Hero.tsx
+++ exercice-front/components/ui/Hero.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const Hero: React.FC = () => (
  <div className="py-20 px-3 md:px-0 max-w-5xl mx-auto" id="home">
       <h1 className="text-3xl md:text-5xl font-bold text-black dark:text-violet-300 text-center mb-16">
-        Welcome to My chalenge Frontend par{" "}
-        <span className="text-violet-600 dark:text-violet-400">IDUN-GROUPE</span>
+        Exercice Frontend -- {" "}
+        <span className="text-violet-600 dark:text-violet-400">IDUN-GROUP</span>
       </h1>
 </div>
 );
